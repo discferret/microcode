78185926a1c5671c757da7eb88d747819d08a434
Philip Pemberton <philpem@philpem.me.uk>
2012-01-07 15:47:08 +0000

    New disc reader engine
    The old disc reader had major problems with race conditions. Specifically, if the counter overflowed at exactly the same time a data or index pulse arrived, the overflow byte would be stored but the timing byte would be discarded. This is fairly hard to spot, but one giveaway is a large quantity of outliers on the scatter plot, offset approx. 128 clocks (or a modulo thereof) from expected. The term for this is "waveform ghosting" (because it looks like there's a ghost copy of the waveform above the original).
    
    This changeset includes a completely rewritten disc reader engine, which has been extensively tested with Icarus Verilog and GTKWave, and SignalTap II v9.1. Friends don't let friends use Quartus 10 or 11. Big thanks to marshallh for the FTP links to Quartus 9.1sp2, which I will repeat for everyone's benefit:
      ftp://ftp.altera.com/outgoing/release/91sp2_quartus_free.exe        (Windows)
      ftp://ftp.altera.com/outgoing/release/91sp2_quartus_free_linux.tar  (Linux)
    
    Also, word to the wise -- don't try and use Post-Fit nodes with SignalTap unless you have the full Subscription Edition of Quartus... you'll corrupt your project file and database. Use Pre-Synthesis nodes instead. If you have used Post-Fit Nodes and built your project (which will fail), turn off SignalTap, then rebuild. Add some Presynth nodes and when SignalTap asks you to change the project mode, say "yes".

--- DiscReader.v
+++ DiscReader.v
@@ -1,130 +1,88 @@
-/**************
- * DiscReader: Data Acquisition Module for floppy disc reader
+/****************************************************************************
+ * DiscReader.v: Magnetic disc reader module.
+ * Philip Pemberton, 2012.
  *
- * P. Pemberton, 2009.
+ * This is a complete rewrite of the original DiscFerret MDR module, and fixes
+ * a (rather large) number of race conditions and ambiguities which caused
+ * data corruption issues when 
  *
- * A complete rewrite of the "DAS1" module, based on a state machine instead
- * of random logic. Slightly less register-efficient, but far easier to
- * maintain, and more reliable too.
+ * This version both has a Verilog testbench, and has been tested against it.
+ * Acquisition is proven to work (using Icarus Verilog and GTKWave).
  *
- * TODO: Timing analysis -- step through this in a simulator under various
- *       conditions, and make sure the timer is accurately measuring the
- *       deltas between flux transition pulses.
- *
- *       Also need to check the logic -- does this code react to the same
- *       flux transition multiple times (if the pulse is longer than 1tcy)?
- *
- *       If so, and a SYNC1tcy element is added to counteract this, what
- *       happens if there's a flux transition mid-way through the TMROVFL
- *       path?
  */
 
 module DiscReader(CLOCK, CLKEN, RUN, FD_RDDATA_IN, FD_INDEX_IN, RESET, DATA, WRITE);
-	input					CLOCK;				// counter clock
-	input					CLKEN;				// counter clock enable
-	input					RUN;					// enable input -- 1 to acquire
-	input					FD_RDDATA_IN;		// read data
-	input					FD_INDEX_IN;		// index pulse
-	input					RESET;				// asynchronous reset
-	
-	output reg	[7:0]	DATA;					// data output to RAM
-	output				WRITE;				// write output to RAM
+	parameter					BITS		= 8;		// data bits
+
+	input							CLOCK;				// counter clock
+	input							CLKEN;				// counter clock enable
+	input							RUN;					// enable input -- 1 to acquire
+	input							FD_RDDATA_IN;		// read data
+	input							FD_INDEX_IN;		// index pulse
+	input							RESET;				// asynchronous reset
+
+	output reg	[BITS-1:0]	DATA;					// data output to RAM
+	output reg					WRITE;				// write output to RAM
+
 
 /////////////////////////////////////////////////////////////////////////////
 // Input synchronisation
-	wire FD_RDDATA_IN_tcysync, FD_RDDATA_IN_tcysync2;
-	Flag_Delay1tcy_OneCycle _fcd_rddata1(CLOCK, FD_RDDATA_IN, FD_RDDATA_IN_tcysync);
-	Flag_Delay1tcy_OneCycle _fcd_rddata2(CLOCK, FD_RDDATA_IN_tcysync, FD_RDDATA_IN_tcysync2);
-
+	wire FD_RDDATA_IN_tcysync, FD_INDEX_IN_tcysync;
+	Flag_Delay1tcy_OneCycle _fcd_rddata	(CLOCK,	FD_RDDATA_IN,	FD_RDDATA_IN_tcysync);
+	Flag_Delay1tcy_OneCycle _fcd_index	(CLOCK,	FD_INDEX_IN,	FD_INDEX_IN_tcysync);
 
 /////////////////////////////////////////////////////////////////////////////
-// Transition period timer
-	wire			ResetTimer;
-	reg		[6:0]	timer;
-	always @(posedge CLOCK or posedge ResetTimer) begin
-		if (ResetTimer) begin
-			// reset -- clear the counter's internal state
-			timer <= 7'd1;
-		end else begin
-			// clock pulses must be qualified by a clock enable
-			if (CLKEN) begin
-				// clock pulse -- increment the timer
-				if (timer == 7'b111_1111) begin
-					// timer overflow
-					timer <= 7'd1;
-				end else begin
-					// increment normally
-					timer <= timer + 7'd1;
-				end
+// Frequency counter
+
+reg[BITS:0] trap;
+
+	// current counter value
+	reg [BITS-2:0] counter;
+	// has the counter overflowed?
+	wire counter_overflow = (counter == ('d1 << BITS-1) - 'd2);
+	// state of INDEX in the previous clock cycle
+	reg last_index_state;
+
+	always @(posedge CLOCK) begin
+		// clear write flag if it is set
+		WRITE <= 1'b0;
+		// save index state from this cycle
+		last_index_state <= FD_INDEX_IN_tcysync;
+
+		// -- frequency counter --
+		if (RESET || !RUN) begin
+			// reset active -- clear counter
+			counter <= 'd0;
+		end else if (CLKEN) begin
+			// otherwise increment
+			if (counter < ('d1 << BITS-1) - 'd2) begin
+				counter <= counter + 'd1;
+			end else begin
+				counter <= 'd0;
+			end
+		end
+
+		if (!RESET && RUN) begin
+			if (counter_overflow && !(FD_RDDATA_IN_tcysync | FD_INDEX_IN_tcysync)) begin
+				// Counter overflow, but RD_DATA and INDEX are inactive. Write an
+				// overflow byte.
+				DATA <= ('d1 << BITS-1) - 'd1;
+				WRITE <= 'd1;
+			end else if (FD_RDDATA_IN_tcysync | FD_INDEX_IN_tcysync) begin
+				// No overflow, either RDDATA or INDEX is active.
+				// Write current counter value
+				DATA[BITS-2:0] <= counter;
+				WRITE <= 1'b1;
+
+				// Set index state bit
+				DATA[BITS-1] <= (last_index_state | FD_INDEX_IN_tcysync);
+
+				// Reset the counter
+				counter <= 'd0;
 			end
 		end
 	end
 
-
-/////////////////////////////////////////////////////////////////////////////
-// State machine
-
-	// FSM states
-	parameter	ST_IDLE		= 2'd0;
-	parameter	ST_WAITDAT	= 2'd1;
-	parameter	ST_WRITE	= 2'd2;
-	
-	// Current state
-	reg		[1:0]	state;
-	
-	always @(posedge CLOCK or posedge RESET) begin
-		if (RESET) begin
-			// Reset -- clear to initial state
-			state <= ST_IDLE;
-		end else if (!RUN) begin
-			// RUN inactive -- go back to idle state
-			state <= ST_IDLE;
-		end else begin
-			case (state)
-				ST_IDLE:	begin
-								// ST_IDLE: Idle state
-								if (!RUN) begin
-									// RUN inactive, keep spinning in the IDLE state
-									state <= ST_IDLE;
-								end else begin
-									// RUN active -- start timing
-									state <= ST_WAITDAT;
-								end
-							end
-							
-				ST_WAITDAT:	begin
-								// ST_WAITDAT: Wait for DATA pulses (flux transitions)
-								if (FD_RDDATA_IN_tcysync) begin
-									// Flux transition detected. Store timer value.
-									DATA <= {FD_INDEX_IN, timer};
-									state <= ST_WRITE;
-								end else if (timer == 7'b111_1111) begin
-									// Timer is going to overflow.
-									// The timer will handle this itself, but we need to store
-									// a special value in the acq buffer to signify that an
-									// overflow has occurred.
-									DATA <= {FD_INDEX_IN, 7'd0};
-									state <= ST_WRITE;
-								end else begin
-									// No transition; keep waiting
-									state <= ST_WAITDAT;
-								end
-							end
-							
-				ST_WRITE:	begin
-								// ST_WRITE: Write the timing byte to RAM. Waits for the FT pulse
-								// to drop first. Write signal is generated elsewhere.
-								state <= ST_WAITDAT;
-							end
-			endcase
-		end
-	end
-	
-	// WRITE is active during the two RAM write states -- Timer Overflow and Write Timing.
-	assign WRITE = (state == ST_WRITE);
-
-	// Reset the timer in the IDLE state, when there's a transition, and when RESET is active
-	assign ResetTimer = (state == ST_IDLE) || (FD_RDDATA_IN_tcysync2) || (RESET);
 endmodule
 
-// vim ts=3 sw=3
+// vim: ts=3 sw=3
